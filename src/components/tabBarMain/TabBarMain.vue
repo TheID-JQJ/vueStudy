14013cd9cd2aaf811a73042c2a10818b79798e8a
激青衿 <hkchenjianhui@foxmail.com>
2022-01-17 15:44:33 +0800

    test

--- src/components/tabBarMain/TabBarMain.vue
+++ src/components/tabBarMain/TabBarMain.vue
@@ -5,12 +5,12 @@
       <tab-bar-item link="/home">
         <img src="~@/assets/image/home.png" slot="item-icon" />
         <img src="~@/assets/image/home-active.png" slot="item-icon-active" />
-        <div slot="item-text">首页</div>
+        <div slot="item-text">home</div>
       </tab-bar-item>
       <tab-bar-item link="/find">
         <img src="~@/assets/image/find.png" slot="item-icon" />
         <img src="~@/assets/image/find-active.png" slot="item-icon-active" />
-        <div slot="item-text">发现</div>
+        <div slot="item-text">find</div>
       </tab-bar-item>
       <tab-bar-item link="/shopcart">
         <img src="~@/assets/image/shopcart.png" slot="item-icon" />
@@ -18,12 +18,12 @@
           src="~@/assets/image/shopcart-active.png"
           slot="item-icon-active"
         />
-        <div slot="item-text">购物车</div>
+        <div slot="item-text">shopping cart</div>
       </tab-bar-item>
       <tab-bar-item link="/profile">
         <img src="~@/assets/image/profile.png" slot="item-icon" />
         <img src="~@/assets/image/profile-active.png" slot="item-icon-active" />
-        <div slot="item-text">我的</div>
+        <div slot="item-text">profile</div>
       </tab-bar-item>
     </tab-bar>
   </div>
